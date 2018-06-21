1043b7dde2b22984e589e6b541a34d0900a4f3a7
Jordan Hayashi <jordan@kensho.com>
2018-06-21 09:21:57 -0400

    Use non-deprecated brew install flag

--- scripts/install_mac_programs.sh
+++ scripts/install_mac_programs.sh
@@ -1,7 +1,7 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 . $DIR/maybe_install_brew.sh
 
-brew install macvim --override-system-vim
+brew install macvim --with-override-system-vim
 
 # https://github.com/creationix/nvm
 curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.8/install.sh | bash
