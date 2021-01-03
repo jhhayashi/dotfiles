68f140f8600a7aef367439063d29a8c1b12f51e9
Jordan Hayashi <jhhayashi@gmail.com>
2021-01-03 15:04:47 -0500

    Update homebrew install with new recommended script

--- scripts/maybe_install_brew.sh
+++ scripts/maybe_install_brew.sh
@@ -1,4 +1,4 @@
 if ! [ `which brew` ]
 then
-  /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 fi
