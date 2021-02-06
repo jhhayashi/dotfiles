6cc882d7861c1433341559fe031e63d28a877407
Jordan Hayashi <jhhayashi@gmail.com>
2021-02-06 15:36:43 -0500

    Default install.sh to showing help options and add dry run ability

--- install.sh
+++ install.sh
@@ -1,12 +1,18 @@
+set -euo pipefail
+
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-if [[ $* == "--help" ]] || [[ $* == "-h" ]]
-then
+if [[ $* == "--help" ]] || [[ $* == "-h" ]] || [[ $# -eq 0 ]]; then
   echo "usage: ./install.sh [ -h | --help ] [ --link | -l ]"
-  echo "-h, --help | show this help"
-  echo "-l, --link | link rc files rather than copying"
+  echo "-h, --help      | show this help"
+  echo "-l, --link      | link rc files rather than copying"
+  echo "-o, --overwrite | copy files into locations, overwriting existing files"
+  echo "--append | append these config files to any config files that already exist"
+  echo "--dry-run | check to see if any config files will be changed"
   exit 0
 fi
+
+set -x
 
 CONFIG_FILES=(
   ".bash_profile"
@@ -15,15 +21,34 @@ CONFIG_FILES=(
   ".tmux.conf"
 )
 
-if [[ $* == "--link" ]] || [[ $* == "-l" ]]
+if [[ $* == "--dry-run" ]]
+then
+  set +x
+  echo -e "\nsearching for existing config files..."
+  for file in "${CONFIG_FILES[@]}"
+  do
+    if [[ -e ~/$file ]]; then
+      echo "~/$file already exists, this script may make unrecoverable changes to it"
+    fi
+  done
+  exit 0
+elif [[ $* == "--link" ]] || [[ $* == "-l" ]]
 then
   echo -e "\nlinking config files..."
   for file in "${CONFIG_FILES[@]}"
   do
     ln -s $DIR/$file ~/$file
   done
-else
-  echo -d "\ncopying config files..."
+elif [[ $* == "--overwrite" ]] || [[ $* == "-o" ]]
+then
+  echo -d "\noverwriting config files..."
+  for file in "${CONFIG_FILES[@]}"
+  do
+    cp $DIR/$file ~/$file
+  done
+elif [[ $* == "--append" ]]
+then
+  echo -d "\nappending config files..."
   for file in "${CONFIG_FILES[@]}"
   do
     cat $DIR/$file >> ~/$file
