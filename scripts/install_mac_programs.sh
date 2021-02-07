18d2809054fd8345b9a452cbd6665906508d1325
Jordan Hayashi <jhhayashi@gmail.com>
2021-02-06 19:44:17 -0500

    Install vim instead of macvim, fix nvm usage, and attempt to install xcode tools

--- scripts/install_mac_programs.sh
+++ scripts/install_mac_programs.sh
@@ -1,8 +1,12 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 . $DIR/maybe_install_brew.sh
 
-brew install macvim --with-override-system-vim
+xcode-select --install
+
+brew install vim
 
 # https://github.com/nvm-sh/nvm
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.37.2/install.sh | bash
-nvm install --lts
+# allow us to run nvm
+source ~/.nvm/nvm.sh
+nvm install --lts --default
