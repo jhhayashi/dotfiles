1bb7e5256cfad042b8a4395232a42c1c7997ea89
Jordan Hayashi <jhhayashi@gmail.com>
2022-07-19 14:48:01 -0400

    Use ALE instead of syntastic

--- scripts/install_vim_plugins.sh
+++ scripts/install_vim_plugins.sh
@@ -56,6 +56,9 @@ git clone https://github.com/christoomey/vim-tmux-navigator
 cd ~/.vim/bundle
 git clone git://github.com/digitaltoad/vim-pug.git
 
+# https://github.com/dense-analysis/ale
+git clone https://github.com/dense-analysis/ale.git ~/.vim/bundle/ale
+
 # https://github.com/xuhdev/vim-latex-live-preview
 cd ~/.vim
 mkdir -p plugin
