39b7651182a917772ae4ff93e40a724a1107fce1
Jordan Hayashi <jhhayashi@gmail.com>
2022-10-28 16:19:10 -0400

    Set macos settings via script

---
+++ scripts/set_macos_settings.sh
@@ -0,0 +1,21 @@
+# key repeating
+defaults write NSGlobalDomain ApplePressAndHoldEnabled -bool false
+defaults write NSGlobalDomain KeyRepeat -int 2
+defaults write NSGlobalDomain InitialKeyRepeat -int 15
+
+# keyboard behavior
+defaults write NSGlobalDomain NSAutomaticCapitalizationEnabled -bool false
+defaults write NSGlobalDomain NSAutomaticDashSubstitutionEnabled -bool false
+defaults write NSGlobalDomain NSAutomaticPeriodSubstitutionEnabled -bool false
+defaults write NSGlobalDomain NSAutomaticQuoteSubstitutionEnabled -bool false
+defaults write NSGlobalDomain NSAutomaticSpellingCorrectionEnabled -bool false
+
+# enable tap to click for this user and for the login screen
+defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad Clicking -bool true
+defaults -currentHost write NSGlobalDomain com.apple.mouse.tapBehavior -int 1
+defaults write NSGlobalDomain com.apple.mouse.tapBehavior -int 1
+
+defaults write com.apple.Safari SendDoNotTrackHTTPHeader -bool true
+
+# fast mouse / trackpad?
+defaults write NSGlobalDomain com.apple.trackpad.scaling -int 3
