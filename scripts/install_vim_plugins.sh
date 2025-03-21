2723dd903d0715ca0e74ee54cd85f092f7fff338
Jordan Hayashi <jhhayashi@gmail.com>
2025-03-21 19:43:22 -0400

    Remove most vim plugins

--- scripts/install_vim_plugins.sh
+++ scripts/install_vim_plugins.sh
@@ -21,29 +21,8 @@ cd ~/.vim/bundle && git clone https://github.com/Valloric/YouCompleteMe.git
 cd YouCompleteMe && git submodule update --init --recursive && \
   ./install.py --clang-completer --ts-completer
 
-# https://github.com/metakirby5/codi.vim
-cd ~/.vim/bundle && git clone https://github.com/metakirby5/codi.vim.git
-if [[ $* == *--update-rc* ]]
-then
-  cat $DIR/.codi >> ~/.bash_profile
-fi
-
 # https://github.com/sheerun/vim-polyglot
 git clone https://github.com/sheerun/vim-polyglot ~/.vim/bundle/vim-polyglot
-
-# https://github.com/JamshedVesuna/vim-markdown-preview
-cd ~/.vim/bundle
-git clone https://github.com/JamshedVesuna/vim-markdown-preview
-brew install grip
-if [[ $* == *--update-rc* ]]
-then
-  echo 'let vim_markdown_preview_github=1' >> ~/.vimrc
-  echo "let vim_markdown_preview_browser='Google Chrome'" >> ~/.vimrc
-fi
-
-# http://vimawesome.com/plugin/jshint
-cd ~/.vim/bundle
-git clone https://github.com/jshint/jshint
 
 # https://github.com/Yggdroot/indentLine
 cd ~/.vim/bundle
@@ -53,25 +32,8 @@ git clone https://github.com/Yggdroot/indentLine
 cd ~/.vim/bundle
 git clone https://github.com/christoomey/vim-tmux-navigator
 
-cd ~/.vim/bundle
-git clone https://github.com/digitaltoad/vim-pug.git
-
 # https://github.com/dense-analysis/ale
 git clone https://github.com/dense-analysis/ale.git ~/.vim/bundle/ale
 
-# https://github.com/tomasr/molokai
-git clone https://github.com/tomasr/molokai ~/.vim/bundle/molokai
-mkdir -p ~/.vim/colors
-cp ~/.vim/bundle/molokai/colors/molokai.vim ~/.vim/colors/
-
 # https://github.com/github/copilot.vim
 git clone https://github.com/github/copilot.vim ~/.vim/bundle/copilot.vim
-
-# https://github.com/xuhdev/vim-latex-live-preview
-cd ~/.vim
-mkdir -p plugin
-git clone https://github.com/xuhdev/vim-latex-live-preview
-mv vim-latex-live-preview/plugin/latexlivepreview.vim plugin/
-rm -rf vim-latex-live-preview
-brew install evince
-brew install --cask mactex
